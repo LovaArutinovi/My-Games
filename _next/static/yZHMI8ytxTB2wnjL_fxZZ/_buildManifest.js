0745d0fb80835457337fbb003e4885527847533c
LovaArutinovi <93638744+LovaArutinovi@users.noreply.github.com>
2022-06-12 20:10:53 +0000

    Deploying to gh-pages from @ LovaArutinovi/My-Games@a60a50c2ca7fff65ebac8f1494716bdd4ffef587 🚀

---
+++ _next/static/yZHMI8ytxTB2wnjL_fxZZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/858-d4ee4119f0e9c056.js","static/chunks/pages/index-c134c1d3a9fb6640.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
